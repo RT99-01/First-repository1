3557d28bcd8f57ed67fb9a9e3d99ff125ddd7d1d
RT99-01 <kang67999@gmail.coM>
2024-09-11 15:45:11 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,8 @@
-def print_hello():
-    print("Hello World!!")
+
 
 def print_KIT():
- print("Kumoh National Institute of Technology.rtrt99")
+ print("KIT")
 
 if __name__ == '__main__':
     print_KIT()
-    print_hello()
+    
